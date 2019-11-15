7f3d215b1d192daabd811852f9708e8cb8a1cced
Reduichi <q2339198@gmail.com>
2019-11-15 23:16:33 +0800

    小雞事件作業

---
+++ UnityProject/Assets/Chicken.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 100)]
+    public float Speed;
+    [Header("跳躍高度"), Range(0, 2000)]
+    public int Jump;
+    [Header("對話內容")]
+    public string Talk = "咕咕咕~";
+    [Header("是否取得雞蛋")]
+    public bool Egg;
+
+    private void Start()
+    {
+        print("遊戲開始!!!");
+    }
+    private void Update()
+    {
+        print("咕咕咕~~~");
+    }
+}
